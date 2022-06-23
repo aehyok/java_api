ea84c53de4b6d45acf6b669b1b13f086ca201663
那个曾经的少年回来了 <455043818@qq.com>
2022-06-23 14:27:44 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-//@MapperScan("com.example.demo.mapper")//扫包   DemoApplication在目前的目录结构上，能够自动扫描整个项目的结构包MapperScan
+//@MapperScan("com.example.demo.mapper")
+//扫包   DemoApplication在目前的目录结构上，能够自动扫描整个项目的结构包MapperScan
 public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
