ff5009c70c49d52a0cf55463586965322266f1f3
刘启明 <455043818@qq.com>
2024-07-08 16:23:43 +0800

    feat: swagger server 实现配置文件

--- src/main/java/com/sun/xxm/controller/UserController.java
+++ src/main/java/com/sun/xxm/controller/UserController.java
@@ -35,7 +35,7 @@ public class UserController extends BaseController {
             @Parameter( name = "loginDto", description = "登录实体")
     })
     @PostMapping("login")
-    public boolean Login(@RequestBody LoginDto model) {
+    public User Login(@RequestBody LoginDto model) {
         if(model.getUserName().isEmpty() || model.getPassword().isEmpty())
         {
             throw new ApiException(ResultCodeEnum.FAILED, "用户名或密码不能为空");
@@ -56,7 +56,7 @@ public class UserController extends BaseController {
         var item = userMapper.selectOne(queryWrapper);
         if (item != null) {
             if (Objects.equals(item.getPassword(), model.getPassword())) {
-                return true;
+                return item;
             }
         }
         throw new ApiException(ResultCodeEnum.FAILED, "用户名或密码错误");
--- src/main/java/com/sun/xxm/utils/SpringDocConfig.java
+++ src/main/java/com/sun/xxm/utils/SpringDocConfig.java
@@ -4,16 +4,29 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * spring doc配置
  */
 @Configuration
 public class SpringDocConfig {
+    @Value("${swagger.server}")
+    private String server;
+
     @Bean
     public OpenAPI restfulOpenAPI() {
+        List<Server> listServers = new ArrayList<Server>();
+        listServers.add(new Server().url(server));
+
         return new OpenAPI()
+                .servers(listServers)
                 .info(new Info().title("NCDP无代码开发平台")
                         .description("专为小项目而设计的后端接口服务")
                         .version("v0.0.1"));
