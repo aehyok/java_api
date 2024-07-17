c036659a6be06b10b29f699b0dd60a93c69ede99
那个曾经的少年回来了 <455043818@qq.com>
2024-07-17 13:53:56 +0800

    feat: add sql日志记录

--- src/main/java/com/sun/xxm/XxmApplication.java
+++ src/main/java/com/sun/xxm/XxmApplication.java
@@ -1,5 +1,8 @@
 package com.sun.xxm;
 
+import com.mybatisflex.core.audit.AuditManager;
+import com.mybatisflex.core.audit.ConsoleMessageCollector;
+import com.mybatisflex.core.audit.MessageCollector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +14,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class XxmApplication {
 
 	public static void main(String[] args) {
+		AuditManager.setAuditEnable(true);
+
+		//设置 SQL 审计收集器
+		MessageCollector collector = new ConsoleMessageCollector();
+		AuditManager.setMessageCollector(collector);
+
 		SpringApplication.run(XxmApplication.class, args);
 	}
 }
